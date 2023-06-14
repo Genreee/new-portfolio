d8464adf08308d5e0321648b5f353ce86c6562ca
genre <genreglennmorales@gmail.com>
2023-06-14 20:15:40 +0800

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,28 @@
+import "../styles/header.scss";
+import "../styles/style.scss";
+
+const Header = () => {
+  return (
+    <header className="container">
+      <ul>
+        <li>
+          <a href="#">about</a>
+        </li>
+        <li>
+          <a href="#">skills</a>
+        </li>
+        <li>
+          <a href="#">service</a>
+        </li>
+        <li>
+          <a href="#">projects</a>
+        </li>
+        <li>
+          <a href="#">contact</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
