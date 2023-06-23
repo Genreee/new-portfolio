624fd93f496c8c18f963f2f5c32f243bfcea67eb
genre <genreglennmorales@gmail.com>
2023-06-23 18:04:46 +0800

    techstack

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,26 +1,31 @@
 import "../styles/skills.scss";
 import "../styles/style.scss";
+import Techstack from "./tech-stack";
 
 
 
 const Skills = () => {
   return (
-    <div className="skills container section">
-      <h2 className="skills-title">skills</h2>
+    <div className="skills-wrapper container section">
+      <div className="skills container section">
+        <h2 className="skills-title">skills</h2>
 
-      <p>
-        Proficient in HTML, CSS, and SCSS framework, with excellent knowledge of
-        structured and <br /> responsive design principles
-      </p>
-      <p>
-        7 months of experience in JavaScript, including version control with
-        Git, GitHub, and build <br /> tools like Vite.
-      </p>
-      <p>Familiarity with front-end frameworks like React JS.</p>
-      <p>
-        Strong problem-solving and analytical skills, meticulous attention to
-        detail, and excellent <br /> communication and collaboration abilities.
-      </p>
+        <p>
+          Proficient in HTML, CSS, and SCSS framework, with excellent knowledge
+          of structured and responsive design principles
+        </p>
+        <p>
+          7 months of experience in JavaScript, including version control with
+          Git, GitHub, and build tools like Vite.
+        </p>
+        <p>Familiarity with front-end frameworks like React JS.</p>
+        <p>
+          Strong problem-solving and analytical skills, meticulous attention to
+          detail, and excellent communication and collaboration
+          abilities.
+        </p>
+      </div>
+      <Techstack/>
     </div>
   );
 };
---
+++ src/components/tech-stack.jsx
@@ -0,0 +1,98 @@
+import "../styles/tech-stack.scss";
+import "../styles/style.scss";
+
+const Techstack = () => {
+  return (
+    <div className="tech-stack container section">
+      <h2 className="tech-stack-title">tech-stack</h2>
+
+      <div class="tech-stack-ul-wrapper">
+        <ul className="about__ul">
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/JavaScript-icon.png"
+              alt="JavaScript icon"
+            />
+            JavaScript (ES6+)
+          </li>
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/CSS-icon.png"
+              alt="CSS icon"
+            />
+            CSS
+          </li>
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/HTML5-icon.png"
+              alt="laptop icon"
+            />
+            HTML5
+          </li>
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/Java-icon.png"
+              alt="Java icon"
+            />
+            Java
+          </li>
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/Git-icon.png"
+              alt="Git icon"
+            />
+            Git
+          </li>
+        </ul>
+        <ul className="about__ul">
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/ReactJS-icon.png"
+              alt="ReactJS icon"
+            />
+            ReactJS
+          </li>
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/SCSS-icon.jpg"
+              alt="SCSS icon"
+            />
+            SCSS
+          </li>
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/Vite-icon.jpg"
+              alt="Vite icon"
+            />
+            Vite
+          </li>
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/Python-icon.png"
+              alt="Python icon"
+            />
+            Python
+          </li>
+          <li className="about__list">
+            <img
+              className="about__icon-img"
+              src="/Github-icon.png"
+              alt="Github icon"
+            />
+            Github
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Techstack;
